f9bc0eac42bcf0a88ead0d658b98accf191471b3
Dan Persa <dan.persa@zalando.de>
2015-01-22 12:14:13 +0100

    make the site more responsive

--- app/assets/javascripts/tasks-app/utils/offcanvas.js
+++ app/assets/javascripts/tasks-app/utils/offcanvas.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
   $('[data-toggle="offcanvas"]').click(function () {
     $('.row-offcanvas').toggleClass('active');
-    $('.row-offcanvas').toggleClass('col-xs-1');
-    $('.row-offcanvas').toggleClass('col-xs-6');
   });
 });
